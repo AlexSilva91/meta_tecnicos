effdbe1a701b7545ef2ced9f2df8436152fa8946
AlexSilva91 <alexsilvaalves17@outlook.com>
2025-10-27 14:35:39 -0300

    update

--- tests/buscar_OS_30D.py
+++ tests/buscar_OS_30D.py
@@ -1,3 +1,9 @@
+import sys
+import os
+# Adiciona o diretório raiz do projeto ao caminho do Python
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from app.database.connection import db
 from app.models.expert import Expert
 from app.models.customer import Customer
@@ -176,7 +182,7 @@ if __name__ == "__main__":
     
     app = create_app()
     with app.app_context():
-        for i in range(60):
+        for i in range(90):
             dia = datetime.today() - timedelta(days=i)
             data_str = dia.strftime("%Y-%m-%d")
             print(f"\n📅 Processando OS para a data: {data_str}")
